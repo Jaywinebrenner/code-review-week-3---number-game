fb0da7edc575e6bd8ee6dcfbd5d78be976e235fb
jay thomas winebrenner <jaywinebrenner1@jays-iMac.local>
2019-11-01 08:47:55 -0700

    get business and front end seperated. Test the plumbing.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,21 @@
+//business logic
 
+function beepBoopFunction(num){
 
+ return num;
 
-
-
-
-
-
-
-
-
-
-
+}
 
 
 
 $(document).ready(function(){
-  $("form#inputForm").submit(function(event){
+  $("form#input").submit(function(event){
     event.preventDefault();
 
+    var userNumber = parseInt($("#formInput").val());
+    var result= beepBoopFunction(userNumber);
+
+    $("ul#outputUl").append("<li>" + result + "</li>");
 
   });
 });
