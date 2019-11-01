2c72aadf1aaa6046724584b64ae80c60bee850f8
jay thomas winebrenner <jaywinebrenner1@jays-iMac.local>
2019-11-01 14:34:45 -0700

    add css styleings

--- js/scripts.js
+++ js/scripts.js
@@ -9,20 +9,15 @@ function beepBoopFunction(num){
    for (var i = 0; i <= num; i ++){
      numberArray.push(i);
      var beep = "BEEP!";
-
-
    };
 
    var boopArray = ["BEEP!", "BOOP!", "I'm sorry, Dave. I'm afraid I can't do that."]
    if (numberArray.includes(2)) {
+     for (var i = 0; i <= numberArray.length; i ++){
 
-   function element_replace() {
+     };
 
-      var ele = Array(10, 20, 300, 40, 50);
 
-      ele[ele.map((x, i) => [i, x]).filter(
-        x => x[1] == 300)[0][0]] = 30
-      }
       // console.log(ele); [10,10,30,40,50]
 
      console.log("there is a 2");
