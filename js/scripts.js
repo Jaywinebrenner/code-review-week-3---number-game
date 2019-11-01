f53cc95cf7826a7ff8c9ac93d28289d31fc45832
jay thomas winebrenner <jaywinebrenner1@jays-iMac.local>
2019-11-01 09:21:57 -0700

    add initial javascript logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,20 @@
 //business logic
 
 function beepBoopFunction(num){
+  if (isNaN(num)){
+    return "Please enter a number that currently exists.";
+  }
+   var inputArray = [];
+   for (var i = 0; i <= num; i++){
+     inputArray.push(num[i]);
+     console.log(inputArray);
 
- return num;
+   }
+
+
+  // for(var i = 0; i < sentenceSplit.length; i++){
+  //       outputArray.push(sentenceSplit[i].charAt(0).toUpperCase() + sentenceSplit[i].slice(1));
+
 
 }
 
